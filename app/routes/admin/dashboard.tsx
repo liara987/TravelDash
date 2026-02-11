facc58cfadf5f0094218b0ab2febe39c4a309dbb
Liara Pereira Duarte <lduarte@adagency.local>
2026-02-11 13:46:30 -0300

    feat: implementa cards de status

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,17 +1,47 @@
-import { Header } from "components";
+import { Header, StatsCard } from "components";
 
 const Dashboard = () => {
   const user = { name: "Liara" };
-  const dashboard = {
+  const dashboardStats = {
     totalUsers: 12450,
+    usersJoined: { currentMonth: 218, lastMonth: 176 },
+    totalTrips: 3210,
+    tripCreated: { currentMonth: 150, lastMonth: 250 },
+    userRole: { total: 62, currentMonth: 25, lastMonth: 15 },
   };
+
+  const { totalUsers, usersJoined, totalTrips, tripCreated, userRole } =
+    dashboardStats;
   return (
     <main className="dashboard wrapper">
       <Header
         title={`Bem vindo ${user?.name ?? "Convidado"}`}
         description="Tenha controle das atividades, destinos instagramaveis e populares em tempo real"
       />
-      Dashboard page content
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total de usuários"
+            total={totalTrips}
+            currentMonthCount={tripCreated.currentMonth}
+            lastMonthCount={tripCreated.lastMonth}
+          />
+
+          <StatsCard
+            headerTitle="Total de viagens"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+
+          <StatsCard
+            headerTitle="Usuários ativos"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
     </main>
   );
 };
--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,5 +1,51 @@
-const StatsCard = () => {
-  return <div></div>;
+import { calculateTrendPercentage, cn } from "~/lib/utils";
+
+const StatsCard = ({
+  headerTitle,
+  total,
+  lastMonthCount,
+  currentMonthCount,
+}: StatsCard) => {
+  const { trend, percentage } = calculateTrendPercentage(
+    currentMonthCount,
+    lastMonthCount,
+  );
+  const isDecrement = trend === "decrement";
+  return (
+    <article className="">
+      <h3 className="text-base fort-medium">{headerTitle}</h3>
+      <div className="content">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-4xl">{total}</h2>
+          <div className="flex items-center gap-2">
+            <figure className="flex items-center gap-1">
+              <img
+                className="size-5"
+                src={`/assets/icons/${isDecrement ? "arrow-down-red.svg" : "arrow-up-green.svg"}`}
+                alt="Flecha"
+              />
+              <figcaption
+                className={cn(
+                  "text-sm font-medium",
+                  isDecrement ? "text-red-500" : "text-success-700",
+                )}
+              >
+                {Math.round(percentage)}%
+              </figcaption>
+              <p className="text-sm font-medium text-gray-100 truncate">
+                vs último mês
+              </p>
+            </figure>
+          </div>
+        </div>
+        <img
+          src={`/assets/icons/${isDecrement ? "decrement.svg" : "increment.svg"}`}
+          className="xl:w-32 w-full h-full md:h-32 xl:h-full"
+          alt="Gráfico de tendências"
+        />
+      </div>
+    </article>
+  );
 };
 
 export default StatsCard;
