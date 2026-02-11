c96d08e561a6c97bd207308ec6517c6af0e75068
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2026-02-10 21:05:08 -0300

    feat: usa o header

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,5 +1,12 @@
+import { Header } from "components";
+
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header title="Página de viagens" description="Veja nossos usuários atuais em tempo real" />
+      All users page content
+    </main>
+  );
 };
 
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,16 @@
+import { Header } from "components";
+
 const Dashboard = () => {
-  return <div>Dashboard Page</div>;
+  const user = { name: "Liara" };
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Bem vindo ${user?.name ?? "Convidado"}`}
+        description="Tenha controle das atividades, destinos instagramaveis e populares em tempo real"
+      />
+      Dashboard page content
+    </main>
+  );
 };
 
 export default Dashboard;
