5e2bcbf9a7f19f5d6013e57b768988bd949317ad
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2026-02-12 23:16:48 -0300

    feat: implementa logout

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,12 +1,15 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useLoaderData, useNavigate } from "react-router";
+import { logoutUser } from "~/appwrite/auth";
 import { sidebarItems } from "~/constants";
 import { cn } from "~/lib/utils";
 
 const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
-  const user = {
-    name: "Liara",
-    email: "liaraprogramadora@gmail.com",
-    imageUrl: "/assets/images/liara.jpg",
+  const user = useLoaderData();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logoutUser();
+    navigate("/sign-in");
   };
 
   return (
@@ -37,19 +40,18 @@ const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
         </nav>
 
         <footer className="nav-footer">
-          <img src={user?.imageUrl || "/assets/images/liara.jpg"} alt={user?.name || "Liara"} />
+          <img
+            src={user?.imageUrl || "/assets/images/liara.jpg"}
+            alt={user?.name || "Liara"}
+            referrerPolicy="no-referrer"
+          />
 
           <article>
             <h2>{user?.name}</h2>
             <p>{user?.email}</p>
           </article>
 
-          <button
-            onClick={() => {
-              console.log("logout");
-            }}
-            className="cursor-pointer"
-          >
+          <button onClick={handleLogout} className="cursor-pointer">
             <img src="/assets/icons/logout.svg" className="size-6" alt="Logout" />
           </button>
         </footer>
