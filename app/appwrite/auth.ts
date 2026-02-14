65a742353152d145858cdcc7a4156d31a6963f87
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2026-02-14 00:46:55 -0300

    feat: pega dados do appwrite

--- app/appwrite/auth.ts
+++ app/appwrite/auth.ts
@@ -98,3 +98,19 @@ export const getUser = async () => {
     return null;
   }
 };
+
+export const getAllUsers = async (limit: number, offset: number) => {
+  try {
+    const { documents: users, total } = await database.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.limit(limit), Query.offset(offset)],
+    );
+    if (total === 0) return { users: [], total };
+
+    return { users, total };
+  } catch (error) {
+    console.log("Erro buscando usuarios", error);
+    return { users: [], total: 0 };
+  }
+};
--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,10 +1,91 @@
+import { ColumnDirective, ColumnsDirective, GridComponent } from "@syncfusion/ej2-react-grids";
 import { Header } from "components";
+import { getAllUsers } from "~/appwrite/auth";
+import { cn, formatDate } from "~/lib/utils";
+import type { Route } from "./+types/all-users";
 
-const AllUsers = () => {
+export const loader = async () => {
+  const { users, total } = await getAllUsers(10, 0);
+  return { users, total };
+};
+
+const AllUsers = ({ loaderData }: Route.ComponentProps) => {
+  const { users } = loaderData ?? [];
   return (
-    <main className="dashboard wrapper">
-      <Header title="Página de viagens" description="Veja nossos usuários atuais em tempo real" />
-      All users page content
+    <main className="all-users wrapper">
+      <Header
+        title="Gerenciar usuários"
+        description="Filtre, ordene, e acesse detalhes dos perfis de usuários"
+      />
+
+      <GridComponent dataSource={Array.isArray(users) ? users : []}>
+        <ColumnsDirective>
+          {/* Nome */}
+          <ColumnDirective
+            field="name"
+            headerText="Name"
+            width="200"
+            textAlign="Left"
+            template={(props: UserData) => (
+              <div className="flex items-center gap-1.5 px-4">
+                <img
+                  src={props?.imageUrl || ""}
+                  alt="Foto do usuário"
+                  className="rounded-full size-8 aspect-square"
+                  referrerPolicy="no-referrer"
+                />
+                <span>{props?.name || "sem nome"}</span>
+              </div>
+            )}
+          />
+
+          {/* Email */}
+          <ColumnDirective field="email" headerText="email" textAlign="Left" width="200" />
+
+          {/* Data de cadastro */}
+          <ColumnDirective
+            field="joinedAt"
+            headerText="Data de cadastro"
+            textAlign="Left"
+            width="120"
+            template={(props: { joinedAt: string }) => formatDate(props?.joinedAt)}
+          />
+
+          {/* Etinerario */}
+          <ColumnDirective
+            field="status"
+            headerText="Tipo"
+            textAlign="Left"
+            width="100"
+            template={(props: UserData) =>
+              props ? (
+                <article
+                  className={cn(
+                    "status-column",
+                    props?.status === "user" ? "bg-success-50" : "bg-light-300",
+                  )}
+                >
+                  <div
+                    className={cn(
+                      "size-1.5 rounded-full",
+                      props?.status === "user" ? "bg-success-500" : "bg-gray-500",
+                    )}
+                  />
+
+                  <h3
+                    className={cn(
+                      "font-inter text-xs font-medium",
+                      props?.status === "user" ? "text-success-700" : "text-gray-500",
+                    )}
+                  >
+                    {props?.status || ""}
+                  </h3>
+                </article>
+              ) : null
+            }
+          />
+        </ColumnsDirective>
+      </GridComponent>
     </main>
   );
 };
